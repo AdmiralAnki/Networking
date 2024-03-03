76448cc1c83d0a7fa1a6f199a39548ab9116072c
Ankith <ankith.kirti@nic.in>
2024-03-03 12:32:29 +0530

    add AlamofireImage

--- Package.swift
+++ Package.swift
@@ -11,11 +11,13 @@ let package = Package(
             name: "Networking",
             targets: ["Networking"]),
     ],
-    dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.1")),],
+    dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git",
+                            .upToNextMajor(from: "5.8.1")),
+                   .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.3.0"))],
     targets: [
         .target(
             name: "Networking",
-            dependencies: ["Alamofire"]),
+            dependencies: ["Alamofire","AlamofireImage"]),
         .testTarget(
             name: "NetworkingTests",
             dependencies: ["Networking"]),
